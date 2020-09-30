0b0e87661bf84fc2997fcd86c3f58e1d44b0f209
Vanessa P <vanessa.pereira.o@hotmail.com>
2020-09-30 03:00:02 -0300

    alteração de porta

--- servidor.js
+++ servidor.js
@@ -1,9 +1,9 @@
 const express = require ('express');
 const rotas = require ('./src/rotas/arquivo_rotas');
+const port = process.env.PORT || 3000;
 
 const app = express();
-const port = 3000;
-         rotas(app, express);
+    rotas(app, express);
 
 app.listen(port, () =>{
 console.log(`Example app listening at http://locahost:${port}`)
